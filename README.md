8a72ec07b85616d4d8c53acf251c119200ccf923
Jose Manuel Gomez <jmgomezr@gmail.com>
2019-10-27 11:37:48 +0100

    Empezando con las Views

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sockslevel
+
+## What?
+
+Gestor de niveles de Pilates. Para usuarios VIP, con calcetines de niveles.
--- sockslevel/urls.py
+++ sockslevel/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.current_datetime),
     path('socks/', include('socks.urls')),
     path('admin/', admin.site.urls),
 ]
